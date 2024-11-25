bf3792f9dc7edca3b2f9c9cdb2a79113b8816894
Michael Voronin <voroninma0912@gmail.com>
2024-11-25 23:10:27 +0500

    Tests

--- conftest.py
+++ conftest.py
@@ -1,9 +1,19 @@
 import pytest
+from selenium import webdriver
+from selene import browser
 
-@pytest.fixture(scope="session")
-def browser():
-    print("Браузер")
+
+@pytest.fixture
+def size_browser():
+    browser.config.window_width = 1024
+    browser.config.window_height = 768
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+    browser.open('https://google.com')
+    browser.config.timeout = 20
+
 
     yield
 
-    print("Закрываем браузер")
+    browser.quit()
