1bbfb85e2aeaccd767f977efcf8b6ad7f61d76a5
Michael Voronin <voroninma0912@gmail.com>
2024-11-27 20:25:45 +0500

    Tests

--- test_google_shold_find_selene.py
+++ test_google_shold_find_selene.py
@@ -4,10 +4,11 @@ from selene import browser, be, have
 def test_positive(size_browser):
     browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene'))
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_negative(size_browser):
     browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('опволпаг213743124khfgbn').press_enter()
-    browser.element('aggra').should(have.text('did not match any documents'))
+    browser.element('#center_col').should(have.text('did not match any documents'))
